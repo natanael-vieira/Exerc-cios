7155248c0a1fd1311be0f8133e6977bd7ada5c63
Natanael Vieira <natanaelsales@live.com>
2022-12-27 07:53:17 -0300

    Update index.js

--- Aula04/index.js
+++ Aula04/index.js
@@ -1,6 +1,19 @@
 document.querySelector('#t1').textContent = 'JavaScript - Introdução';
 document.querySelector('#t2').textContent = 'Exemplificando a funcionalidade de .querySelector';
 
-const nome = prompt('Qual é o seu nome?');
+/*const nome = prompt('Qual é o seu nome?');
 
-alert(`Que bom ter você aqui ${nome}! \nVamos começar!`)
+alert(`Que bom ter você aqui ${nome}! \nVamos começar!`)*/
+
+//Função para exibir mensagem de boas-vindas e esconder o formulário
+function bemVindo(){
+    const nome = document.querySelector('#nomeUsuario').value;
+    alert(`Olá ${nome}! Seja bem-vindo!`);
+    document.getElementById('formularioLogin').style.display = 'none';
+}
+
+function mudaCorFundo(){
+    const menu = document.querySelector('#coresFundo');
+    const cor = menu.options[menu.selectedIndex].value;
+    document.body.style.background = cor;
+}
