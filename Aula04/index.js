95cf7840d6b410d11cd931163e6f1f55c87d5f65
Natanael Vieira <natanaelsales@live.com>
2022-12-27 06:51:04 -0300

    Create index.js

---
+++ Aula04/index.js
@@ -0,0 +1,6 @@
+document.querySelector('#t1').textContent = 'JavaScript - Introdução';
+document.querySelector('#t2').textContent = 'Exemplificando a funcionalidade de .querySelector';
+
+const nome = prompt('Qual é o seu nome?');
+
+alert(`Que bom ter você aqui ${nome}! \nVamos começar!`)
