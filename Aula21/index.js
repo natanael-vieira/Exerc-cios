b3a477664d16b6ca401d764a58ca76f97d5d1393
Natanael Vieira <natanaelsales@live.com>
2023-01-04 05:46:49 -0300

    Update index.js

--- Aula21/index.js
+++ Aula21/index.js
@@ -1,6 +1,7 @@
 const listaTarefas = document.querySelector('#listaTarefas');
 const caixaTexto = document.querySelector("#caixaDeTexto");
 const botaoAdicionar = document.querySelector('#botaoAdicionar');
+const listaSuspensa = document.querySelector('#listaSuspensa')
 
 //Listener - sempre que o botao adicionar for clicado, adiciona um item ou uma tarefa
 botaoAdicionar.addEventListener('click', function () {
@@ -8,6 +9,7 @@ botaoAdicionar.addEventListener('click', function () {
     caixaTexto.value = '';
 
     listaTarefas.appendChild(adicionaTarefa(textoDatarefa));
+    exibeOcultaListaSuspensa();
     caixaTexto.focus();
 }
 );
@@ -46,8 +48,33 @@ function acidicionarBotaoRemover() {
     //Listener - sempre que o botão remover for clicado, será removido um item da lista
     botaoRemover.addEventListener('click', function () {
         listaTarefas.removeChild(this.parentNode);
+        exibeOcultaListaSuspensa();
     }
     );
 
     return botaoRemover;
 }
+
+function exibeOcultaListaSuspensa() {
+    const elementoSPAN = document.querySelector('#tarefa');
+    if(elementoSPAN === null) {
+        listaSuspensa.setAttribute('hidden', 'hidden');
+    } else {
+        listaSuspensa.removeAttribute('hidden');
+    }
+}
+
+listaSuspensa.addEventListener('change', function() {
+    if(listaSuspensa.selectedIndex === 1 || listaSuspensa.selectedIndex === 2) {
+        const vetorTarefa = listaTarefas.querySelectorAll('#tarefa');
+        for(tarefa of vetorTarefa) {
+            tarefa.dispatchEvent(new Event('click'));
+        }
+    } else if(listaSuspensa.selectedIndex === 3) {
+        const vetorBotoes = listaTarefas.querySelectorAll('.remover');
+        for(botao of vetorBotoes) {
+            botao.dispatchEvent(new Event('click'));
+        }
+    }
+}
+);
