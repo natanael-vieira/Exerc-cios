ff14f8b7f7a1f889364f9184067889ca25aeb3a8
Natanael Vieira <natanaelsales@live.com>
2023-01-04 05:13:52 -0300

    Update index.js

--- Aula21/index.js
+++ Aula21/index.js
@@ -2,7 +2,8 @@ const listaTarefas = document.querySelector('#listaTarefas');
 const caixaTexto = document.querySelector("#caixaDeTexto");
 const botaoAdicionar = document.querySelector('#botaoAdicionar');
 
-botaoAdicionar.addEventListener('click', function(){
+//Listener - sempre que o botao adicionar for clicado, adiciona um item ou uma tarefa
+botaoAdicionar.addEventListener('click', function () {
     const textoDatarefa = caixaTexto.value;
     caixaTexto.value = '';
 
@@ -11,7 +12,7 @@ botaoAdicionar.addEventListener('click', function(){
 }
 );
 
-function adicionaTarefa (textoDatarefa) {
+function adicionaTarefa(textoDatarefa) {
     const elementoLi = document.createElement('li');
     const elementoSPAN = document.createElement('span');
 
@@ -22,16 +23,17 @@ function adicionaTarefa (textoDatarefa) {
     elementoLi.appendChild(elementoSPAN);
     elementoLi.appendChild(acidicionarBotaoRemover());
 
-    elementoSPAN.addEventListener('click', function() {
-        console.log(elementoLi)
-        if(this.id === 'tarefa') {
-            if(this.parentNode.className === 'naoRealizada') {
+    //Listener - Sempre que um item do mouse for clicado altera o marcador, a cor da fonte e risca o texto
+    elementoSPAN.addEventListener('click', function () {
+        if (this.id === 'tarefa') {
+            if (this.parentNode.className === 'naoRealizada') {
                 this.parentNode.className = 'realizada'
             } else {
                 this.parentNode.className = 'naoRealizada'
             }
         }
-    });
+    }
+    );
 
     return elementoLi;
 }
@@ -41,13 +43,11 @@ function acidicionarBotaoRemover() {
     botaoRemover.textContent = '⊗';
     botaoRemover.className = 'remover'
 
-    console.log(listaTarefas);
-
-    botaoRemover.addEventListener('click', function() {
+    //Listener - sempre que o botão remover for clicado, será removido um item da lista
+    botaoRemover.addEventListener('click', function () {
         listaTarefas.removeChild(this.parentNode);
     }
-
-    )
+    );
 
     return botaoRemover;
 }
